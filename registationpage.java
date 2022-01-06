9dcafb187e49be2f5b4d31c17c2455bac37efa7f
maha <maha@gmail.com>
2022-01-06 06:55:01 +0530

    my reg task completed

--- registationpage.java
+++ registationpage.java
@@ -1,3 +1,10 @@
 this is registation page
 my manager work completed
+1 day lines of code completed
+2 day lines of code completed
+3 day lines of code completed
+4 day lines of code completed 90 %  completed
+5 day line of  code completed 100% completed 
+
+
 
